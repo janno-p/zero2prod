30be8b3c791c753533784ce993c82a00d85a11ad
Janno Põldma <janno.poldma@gmail.com>
2021-04-04 17:24:14 +0300

    Add tracing

--- README.md
+++ README.md
@@ -16,6 +16,7 @@ $ cargo install cargo-audit
 $ cargo install cargo-edit
 $ cargo install cargo-expand
 $ cargo install --locked --version=0.5.1 sqlx-cli --no-default-features --features postgres
+$ cargo install cargo-udeps
 
 $ cargo tarpaulin --ignore-tests
 $ cargo clippy
--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,6 +1,7 @@
 use actix_web::{web, HttpResponse};
 use chrono::Utc;
 use sqlx::PgPool;
+use tracing_futures::Instrument;
 use uuid::Uuid;
 
 #[derive(serde::Deserialize)]
@@ -13,6 +14,18 @@ pub async fn subscribe(
     form: web::Form<FormData>,
     pool: web::Data<PgPool>,
 ) -> Result<HttpResponse, HttpResponse> {
+    let request_id = Uuid::new_v4();
+    let request_span = tracing::info_span!(
+        "Adding a new subscriber.",
+        %request_id,
+        email = %form.email,
+        name = %form.name
+    );
+    let _request_span_guard = request_span.enter();
+
+    let query_span = tracing::info_span!(
+        "Saving new subscriber details in the database"
+    );
     sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
@@ -24,10 +37,12 @@ pub async fn subscribe(
         Utc::now()
     )
     .execute(pool.get_ref())
+    .instrument(query_span)
     .await
     .map_err(|e| {
-        eprintln!("Failed to execute query: {}", e);
+        tracing::error!("Failed to execute query: {:?}", e);
         HttpResponse::InternalServerError().finish()
     })?;
+
     Ok(HttpResponse::Ok().finish())
 }
